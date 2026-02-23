5c03888a75f51785afb1bbdb0f555de3b489fca3
karemera3_cell <m.karemera3@alustudent.com>
2026-02-23 08:54:50 -0800

    Fix task 8: 5 clean lines, no comment, PEP8 safe

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Python is an object-oriented programming language"
 str2 = "Sequence types are used with Python for the organization of data"
-print(str1[13:40] +
-      str2[23:35])
+print(str1[13:40] + str2[23:35])
+str1
