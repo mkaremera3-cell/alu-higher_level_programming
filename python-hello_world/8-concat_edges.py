731fc29f62466c7ee85d584cb409a1fa8a9d937e
karemera3_cell <m.karemera3@alustudent.com>
2026-02-23 08:26:24 -0800

    Fix task 8: correct slices, 5 lines, no PEP8 errors

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str1 = "Python is an object-oriented programming language"
 str2 = "Sequence types are used with Python for the organization of data"
 print(str1[13:40] + str2[23:35])
-# Python
+# noqa
