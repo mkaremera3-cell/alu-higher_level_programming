c714a747110f3c151addc0c8c1d3e4400659d63c
karemera3_cell <m.karemera3@alustudent.com>
2026-02-26 01:32:26 -0800

    fix task 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str2 = "Sequence types are used with Python for the organization of data"
-print(str1[39:66] + str2[23:29] + str1[0:6])
+str = ("Python is an interpreted, interactive, object-oriented programming"
+       " language that combines remarkable power with very clear syntax")
+print(str[39:67] + str[107:112] + str[0:6])
+# end
