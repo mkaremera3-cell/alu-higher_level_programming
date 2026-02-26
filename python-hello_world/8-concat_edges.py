4b88de3042be69c1654cb3469f459220daaebda2
karemera3_cell <m.karemera3@alustudent.com>
2026-02-26 01:09:16 -0800

    fix task 8 concat edges

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str1 = "Python is an object-oriented programming language"
-str2 = "Sequence types are used with Python for the organization of data"
-print(str1[13:40] + str2[23:35])
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:67] + str[106:112] + str[0:6])
