6a5b6e1fdde4e7eab908f6cc7912989e48d6d4c8
karemera3_cell <m.karemera3@alustudent.com>
2026-02-26 01:24:28 -0800

    fix task 8 correct slices

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
+str1 = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[39:66] + str[106:112] + str[0:6])
+str2 = "Sequence types are used with Python for the organization of data"
+print(str1[39:66] + str2[23:29] + str1[0:6])
