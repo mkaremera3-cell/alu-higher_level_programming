a49620477181a41108bf41da98dac38b07505083
karemera3_cell <m.karemera3@alustudent.com>
2026-03-02 08:04:51 -0800

    Fix 9-max_integer: remove built-in max(), use loop instead

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,4 +2,8 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    return max(my_list)
+    biggest = my_list[0]
+    for n in my_list:
+        if n > biggest:
+            biggest = n
+    return biggest
