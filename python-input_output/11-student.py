4c515b9c59662439092aa1d2e85196539ed6dd30
mkaremera3-cell <mkaremera3-cell@gmail.com>
2026-03-11 20:03:09 +0000

    Fix PEP8 on 11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -6,12 +6,13 @@ class Student:
     """Student class"""
 
     def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves dictionary representation of Student with optional filter"""
+        """Retrieves dictionary representation of Student"""
         if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
             return {k: v for k, v in self.__dict__.items() if k in attrs}
         return self.__dict__
