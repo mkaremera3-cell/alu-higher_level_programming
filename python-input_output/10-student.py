9aeab325eb9aeade6bfb8aa2d9970ba43546b080
mkaremera3-cell <mkaremera3-cell@gmail.com>
2026-03-11 20:05:34 +0000

    Fix PEP8 on 10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -6,6 +6,7 @@ class Student:
     """Student class"""
 
     def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
