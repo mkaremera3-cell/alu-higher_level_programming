6a5fbada78015219eeec3e1f8f725e92e2107a47
mkaremera3-cell <mkaremera3-cell@gmail.com>
2026-04-04 13:36:14 +0000

    Fix 4-header.sh: try X-School-User-Id header

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request with X-HolbertonSchool-User-Id header and displays the body
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request with X-School-User-Id header and displays the body
+curl -s -H "X-School-User-Id: 98" "$1"
