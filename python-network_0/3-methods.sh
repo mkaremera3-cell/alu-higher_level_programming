070a3172bd9606f0c365c4a02540d50c7f9b2adc
mkaremera3-cell <mkaremera3-cell@gmail.com>
2026-04-04 13:07:05 +0000

    Fix: add comments and ensure 3 lines per script

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes a URL and displays all HTTP methods the server will accept
+# Displays all HTTP methods the server will accept
 curl -s -X OPTIONS "$1" -i | grep Allow | cut -d' ' -f2-
