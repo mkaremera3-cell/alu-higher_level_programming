1a4a6c2489f45b4de646bfa61142c5b05f1bca24
mkaremera3-cell <mkaremera3-cell@gmail.com>
2026-04-04 13:34:38 +0000

    Fix 4-header.sh: correct header value

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request with a header variable and displays the body
+# Sends a GET request with X-HolbertonSchool-User-Id header and displays the body
 curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
