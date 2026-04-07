438731854a65ecaa4cea73f2f43a31af9a1ac12e
mkaremera3-cell <mkaremera3-cell@gmail.com>
2026-04-07 19:56:26 +0000

    Fix 10-my_github.py: add docstring and PEP8

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -3,5 +3,6 @@
 import requests
 import sys
 
-r = requests.get('https://api.github.com/user', auth=(sys.argv[1], sys.argv[2]))
+r = requests.get('https://api.github.com/user',
+                 auth=(sys.argv[1], sys.argv[2]))
 print(r.json().get('id'))
